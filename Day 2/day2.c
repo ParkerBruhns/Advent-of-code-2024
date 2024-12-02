21ce18b2d8be2dbdc712b01bb5cd9325ca2e3cad
Parker Bruhns <pbruhns2023@my.fit.edu>
2024-12-02 01:30:18 -0500

    Updated day2

--- Day 2/day2.c
+++ Day 2/day2.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_2d_arr(int arr[][1000], int arr_len, int arr_height);
+
 int main() {
    FILE* fptr = fopen("input.txt", "r");
 
-   // 20 colums, 1000 rows
+   // 50 colums, 1000 rows
+   int arr_len = 50;
+   int arr_height = 1000;
    int arr[50][1000];
+
    char line[50];
    int num, count;
    int index = 0;
    while (fgets(line, sizeof(line), fptr)) {
       count = 0;
       char* token = strtok(line, " \t\n");
-      printf("Index: %d\n", index);
       while (token != NULL) {
          if (sscanf(token, "%d", &num) == 1) {
-            printf("\tNumber %d: %d\n", count, num);
             arr[count][index] = num;
             count++;
          } else {
@@ -27,5 +30,22 @@ int main() {
       index++;
    }
 
+   print_2d_arr(arr, arr_len, arr_height);
+
+}
+
+void print_2d_arr(int arr[][1000], int arr_len, int arr_height) {
+   for (int j = 0; j < arr_height; j++) {
+      printf("[");
+      for (int i = 0; i < arr_len; i++) {
+         if (arr[i][j] != 0) {
+            printf("%d", arr[i][j]);
+            if (j != arr_len - 1) {
+               printf(", ");
+            }
+         }
+      }
+      printf("]\n");
+   }
 
 }
